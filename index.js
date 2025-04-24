86f2c32d49dc70ad96c0a11d5ee4321b67177014
Ashley Lin <linashleyy@gmail.com>
2025-04-25 07:11:00 +0800

    Create a function to display order item quantities and console log it

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const userInput = prompt(
   "Enter a list of froyo flavors you would like to order, each separated with a comma: ",
   "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
+
+const froyoFlavors = userInput.split(",");
+
+function displayOrder(froyoFlavors) {
+  const order = {};
+
+  for (const flavor of froyoFlavors) {
+    if (order[flavor]) {
+      order[flavor]++;
+    } else {
+      order[flavor] = 1;
+    }
+  }
+
+  return order;
+}
+
+console.log(displayOrder(froyoFlavors));
